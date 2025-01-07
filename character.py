297707efbf8affcd830f108fecfb2a27ab1df643
Ashraf-DIU <ashrafbialam17@ggmail.com>
2025-01-07 09:57:56 +0600

    Adding character

---
+++ character.py
@@ -0,0 +1,13 @@
+str = "Ashraf Bin Alam"
+# ch = str[0]
+# print(len(str))
+# print(ch)
+
+# Slicing
+# print(str[1:4])
+# print(str[7:len(str)])
+# print(str[:6]) #[0:6]
+# print(str[11:]) #[11:len(str)]
+
+#negative Index
+print(str[-6:-1])
---
+++ python.py
@@ -0,0 +1,9 @@
+str1 = "This is a String. \nI am Ashraf Bin Alam.\t"
+str2 = 'Ashraf Bin Alam'
+str3 = """This is also a String"""
+
+print(str1) 
+# print(str1+str3)
+final_str = str1+str3
+print(final_str)
+print(len(final_str))
